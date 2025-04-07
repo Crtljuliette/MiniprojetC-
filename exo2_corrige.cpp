93aca3a090fc9fb443c5929e9f83eeb7f1e3fa23
Crtljuliette <163295789+Crtljuliette@users.noreply.github.com>
2025-04-07 08:26:53 +0200

    Add files via upload

--- exo2_corrige.cpp
+++ exo2_corrige.cpp
@@ -1,90 +1,26 @@
-#include <random>
 #include <iostream>
-#include <string>
+#include <fstream>
 
-// On prend une liste de mots. Dans un vrai programme, on prendrait soin de
-// séparer le code des données et on lirait cette liste depuis un fichier ou
-// une ressource en ligne.
-const std::vector<std::string> LISTE = {
-	"programme",
-	"informatique",
-	"langage",
-	"computationnelle",
-	"binaire",
-	"flottant",
-	"caractère",
-	"fichier",
-	"compilateur",
-	"interpréteur"
-};
+#include "Biscuit.h"
+#include "BiscuitFourre.h"
 
-const std::string& motAuHasard()
+Biscuit* pointer()
 {
-	// On demande au système d'exploitation une graine
-	std::random_device rd;
-	// pour initialiser un générateur de nombres pseudo-aléatoires,
-	std::default_random_engine gen{rd()};
-	// puis on transforme la sortie de ce générateur pour obtenir une distribution
-	// uniforme couvrant tous les éléments de la liste de mots.
-	std::uniform_int_distribution<> distrib(1, LISTE.size());
+	std::cout << "Instanciation du Biscuit fourré b3 framboise/pistache, diamètre 1cm" << std::endl;
+	Biscuit* b3 = new BiscuitFourre{"framboise", "pistache", 1};
 
-	// On prend un nombre au hasard, on soustrait 1 car les éléments de listes
-	// sont numérotés à partir de 0 et on retourne l'élément de liste
-	// correspondant.
-	return LISTE[distrib(gen) - 1];
-}
+	// Déjà fait !
+	// std::cout << "Création du pointeur sur le biscuit fourré b3" << std::endl;
 
-std::string demanderEntreeUtilisateur()
-{
-	std::string saisie;
-	std::cout << "	> ";
-	std::cin >> saisie;
-	return saisie;
-}
-
-int deroulementJeu(int vies)
-{
-	const std::string& mot = motAuHasard();
-	std::string cache(mot.size(), '_');
-	std::cout << "J'ai choisi un mot !" << std::endl;
-
-	while (vies > 0 && cache != mot) {
-		std::cout << "Le mot à deviner est " << cache << ". "
-			  << "Il vous reste " << vies << " vies."
-			  << std::endl;
-
-
-		std::string saisie = demanderEntreeUtilisateur();
-		if (saisie.size() > 1) {
-			// l'utilisateur a saisi un mot et pas une seule lettre
-			cache = saisie;
-			break;
-		} else {
-			char car = saisie[0];
-			bool presente = false;
-			for (int i=0 ; i<mot.size() ; i++) {
-				if (mot[i] == car) {
-					presente = true;
-					cache[i] = car;
-				}
-			}
-			if (!presente)
-				vies--;
-		}
-	}
-
-	if (cache == mot) {
-		std::cout << "Vous avez gagné ! Le mot à trouver était bien " << mot << ". "
-			  << "Vous marquez " << vies << " points." << std::endl;
-		return vies;
-	} else {
-		std::cout << "Vous avez perdu ! Le mot à trouver était " << mot << ". "
-			  << "Vous marquez 0 point." << std::endl;
-		return 0;
-	}
+	return b3;
 }
 
 int main()
 {
-	deroulementJeu(10);
+	std::cout << "Affichage : Biscuit pointer sur b3" << std::endl;
+	Biscuit* ptr = pointer();
+	std::cout << ptr->donnerDescriptionNonVirtual() << "\n"
+		  << ptr->donnerDescriptionVirtual() << "\n"
+		  << std::endl;
+	delete ptr;
 }
